094e2c7764ee709845b8dbf2df17120aae6e4f5d
AbdulVasay2612 <mdabdulvasayhussain2612@gmail.com>
2024-07-31 11:18:55 +0530

    first commit

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ var usersRouter = require('./app_server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server','views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
